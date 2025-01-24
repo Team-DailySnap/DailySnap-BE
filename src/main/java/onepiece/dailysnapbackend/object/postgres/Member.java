6a6da4e0d7d5692ff47f7c43b2f0574a96d43766
hwangsea <134906042+hwangsea@users.noreply.github.com>
2025-01-24 18:15:58 +0900

    git commit -m "[기능추가][로그인] JWT 로그인 기능 추가: feat: Member 엔티티 설계 및 추가 #6"

---
+++ src/main/java/onepiece/dailysnapbackend/object/postgres/Member.java
@@ -0,0 +1,36 @@
+package onepiece.dailysnapbackend.object.postgres;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Member {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long memberId;
+
+  @Column(unique = true, nullable = false)
+  private String username;
+
+  @Column(nullable = false)
+  private String password;
+
+  @Column(unique = true, nullable = false)
+  private String nickname;
+}
+
