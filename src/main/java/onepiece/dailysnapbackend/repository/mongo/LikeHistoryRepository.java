3b5a108276fc0511562abe9f6b2d547c90784c44
Kim na young <kimna041208@gmail.com>
2025-03-08 03:02:41 +0900

    [기능요청][좋아요] 게시글 좋아요 기능 추가 feat: 게시물 조회수, 좋아요 증가 로직 추가

--- src/main/java/onepiece/dailysnapbackend/repository/mongo/LikeHistoryRepository.java
+++ src/main/java/onepiece/dailysnapbackend/repository/mongo/LikeHistoryRepository.java
@@ -2,9 +2,9 @@ package onepiece.dailysnapbackend.repository.mongo;
 
 import java.util.UUID;
 import onepiece.dailysnapbackend.object.mongo.LikeHistory;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface LikeHistoryRepository extends JpaRepository<LikeHistory, UUID> {
+public interface LikeHistoryRepository extends MongoRepository<LikeHistory, UUID> {
 
   Boolean existsByPostIdAndMemberId(UUID postId, UUID memberId);
 
