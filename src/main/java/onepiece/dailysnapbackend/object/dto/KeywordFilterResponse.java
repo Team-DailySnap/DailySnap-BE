ee87331c06bc339a3d2b9eb08dbce3ad41dba54e
hwangsea <134906042+hwangsea@users.noreply.github.com>
2025-03-19 01:13:43 +0900

    [기능추가][인기 게시물] 키워드 dto 수정 #72

--- src/main/java/onepiece/dailysnapbackend/object/dto/KeywordFilterResponse.java
+++ src/main/java/onepiece/dailysnapbackend/object/dto/KeywordFilterResponse.java
@@ -1,6 +1,7 @@
 package onepiece.dailysnapbackend.object.dto;
 
 import java.time.LocalDate;
+import java.util.UUID;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -15,6 +16,7 @@ import onepiece.dailysnapbackend.object.constants.KeywordCategory;
 @NoArgsConstructor
 public class KeywordFilterResponse {
 
+  private UUID keywordId; // 키워드 ID
   private String keyword; // 키워드 텍스트 필터
   private KeywordCategory category; // 카테고리 필터
   private LocalDate providedDate; // 날짜 필터
