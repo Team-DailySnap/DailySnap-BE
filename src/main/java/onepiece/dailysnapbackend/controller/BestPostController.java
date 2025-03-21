ccdd34518682388c5979ef453a187205677adfdf
hwangsea <134906042+hwangsea@users.noreply.github.com>
2025-03-21 12:59:31 +0900

    [기능추가][인기 게시물] 인기 게시물 controller 추가 #72

--- src/main/java/onepiece/dailysnapbackend/controller/BestPostController.java
+++ src/main/java/onepiece/dailysnapbackend/controller/BestPostController.java
@@ -2,18 +2,19 @@ package onepiece.dailysnapbackend.controller;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
-import java.time.LocalDate;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import onepiece.dailysnapbackend.object.dto.BestPostRequest;
+import onepiece.dailysnapbackend.object.dto.BestPostResponse;
 import onepiece.dailysnapbackend.object.dto.CustomUserDetails;
-import onepiece.dailysnapbackend.object.postgres.Post;
 import onepiece.dailysnapbackend.service.BestPostService;
+import onepiece.dailysnapbackend.util.log.LogMonitoringInvocation;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -24,17 +25,16 @@ import org.springframework.web.bind.annotation.RestController;
     name = "인기 게시물 API",
     description = "일간, 주간, 월간 인기 게시물 API 제공"
 )
-public class BestPostController implements BestPostControllerDocs{
+public class BestPostController implements BestPostControllerDocs {
+
   private final BestPostService bestPostService;
 
   @Override
   @GetMapping
-  public ResponseEntity<List<Post>> getBestPosts(
+  @LogMonitoringInvocation
+  public ResponseEntity<List<BestPostResponse>> getBestPosts(
       @AuthenticationPrincipal CustomUserDetails customUserDetails,
-      @Valid @RequestParam String filter,
-      @Valid @RequestParam LocalDate startDate) {
-
-    List<Post> bestPosts = bestPostService.getBestPosts(filter, startDate);
-    return ResponseEntity.ok(bestPosts);
+      @Valid @ModelAttribute BestPostRequest request) {
+    return ResponseEntity.ok(bestPostService.getBestPosts(request));
   }
 }
--- src/main/java/onepiece/dailysnapbackend/controller/BestPostControllerDocs.java
+++ src/main/java/onepiece/dailysnapbackend/controller/BestPostControllerDocs.java
@@ -1,31 +1,33 @@
 package onepiece.dailysnapbackend.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
-import java.time.LocalDate;
 import java.util.List;
+import onepiece.dailysnapbackend.object.dto.BestPostRequest;
+import onepiece.dailysnapbackend.object.dto.BestPostResponse;
 import onepiece.dailysnapbackend.object.dto.CustomUserDetails;
-import onepiece.dailysnapbackend.object.postgres.Post;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 public interface BestPostControllerDocs {
 
   @Operation(
       summary = "인기 게시물 조회",
       description = """
-                인기 게시물을 조회합니다.
-                
-                ### 요청 파라미터
-                - **filter** (String) : 필터 기준 (daily, weekly, monthly)
-                - **startDate** (LocalDate) : 시작 날짜 (YYYY-MM-DD)
-                
-                ### 반환값
-                - **List<Post>** : 인기 게시물 리스트
-            """
+          이 API는 인기 게시물 목록을 조회하는 API입니다.
+
+          ### 요청 파라미터
+          - **filter** (String) : 게시물 필터 유형 (일간, 주간, 월간)
+          - **startDate** (LocalDate) : 게시물 조회 시작 날짜 (YYYY-MM-DD)
+          
+          ### 반환값
+          - **postId** (UUID) : 게시물 ID
+          - **viewCount** (Integer) : 조회수
+          - **likesCount** (Integer) : 좋아요 수
+          - **createdDate** (LocalDate) : 생성일
+          """
   )
-  @GetMapping
-  ResponseEntity<List<Post>> getBestPosts(CustomUserDetails customUserDetails,
-      @RequestParam String filter,
-      @RequestParam LocalDate startDate);
+  ResponseEntity<List<BestPostResponse>> getBestPosts(
+      CustomUserDetails customUserDetails,
+      @ModelAttribute BestPostRequest request
+  );
 }
